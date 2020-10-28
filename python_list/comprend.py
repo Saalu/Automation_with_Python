5838d903216ad04798d2d63d7affa8bcb3757f17
unknown <saaluissaka>
2020-10-28 10:34:54 +0000

    python list

---
+++ python_list/comprend.py
@@ -0,0 +1,3 @@
+cubes = [i**3 for i in range(10) if i%2 == 0]
+
+print(cubes)
---
+++ python_list/slice.py
@@ -0,0 +1,14 @@
+words =['Hello', 'world', '!']
+print(words, type(words))
+
+print(words[1])
+m = [[1,2,3], [4,5,6]]
+print(m[1][2])
+
+squares = [0,1,4,9,16,25,36, 49, 64]
+print(squares[2:6])
+print(squares[6:])
+print(squares[:6])
+print(squares[::2])
+print(squares[2:6:3])
+print(squares[::-1])
