d089457a5c94ba4ca24a9778f03930008e346b57
saalu issaka <saaluissaka@gmail.com>
2025-07-10 13:13:07 +0000

    syntax and semantics

---
+++ a-refresh-2025/start.py
@@ -0,0 +1,5 @@
+x = "awesoome"
+def myfunc():
+    x = "fantastic"
+    print("Learn new things " + x)
+myfunc()
---
+++ a-refresh-2025/strings.py
@@ -0,0 +1,17 @@
+a = """Lorem ipsum dolor sit amet,consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."""
+# print(a)
+
+# String Length
+a = "Hello, World!"
+# print(len(a))
+
+#Check if "free" is present in the following text:
+txt = "The best things in life are free!"
+# print("free" in txt)
+
+# Print only if "free" is present:
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
