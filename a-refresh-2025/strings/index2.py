8c95c750a5d20dc7d01ab816269caece41b18772
saalu issaka <saaluissaka@gmail.com>
2025-07-14 14:16:37 +0000

    extra tips on strings

---
+++ a-refresh-2025/strings/index2.py
@@ -0,0 +1,18 @@
+a = "Hello, World!"
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())
+# print(a.replace("o", "J"))
+# print(a.split(","))
+
+age = 36
+# txt = "My name is Saalu, I am " + str(age)
+txt = f"My name is Saalu, I am {age} "
+tet = "We are the so-called \"Vikings\" from the north."
+text = "Hello\nWorld"
+x = 'Welcome'
+# print(x[3:5])
+x = 9
+y = 2
+print(f'The price is {x + y} dollars')
+print(bool(0))
