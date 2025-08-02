54dae86b44902a37d009545fb084a4bc36df64eb
saalu issaka <saaluissaka@gmail.com>
2025-08-02 11:43:14 +0000

    class and atrributes

---
+++ a-refresh-2025/dictionary/class/index.py
@@ -0,0 +1,18 @@
+class Apple:
+    pass
+
+class Apple:
+    color = ""
+    flavor = ""
+gold = Apple()
+gold.color = "yellow"
+gold.flavor = 'sweet'
+print(gold.color.upper())
+
+# another instance
+jona = Apple()
+jona.color = "red"
+jona.flavor = 'soft'
+
+print(jona.color.upper())
+
